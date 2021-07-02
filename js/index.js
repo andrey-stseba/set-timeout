17e29e6d93127882345c79b7a2cd5768774cf63b
andrey-stseba <scebaandru@gmail.com>
2021-07-02 20:31:08 +0300

    Initial project version

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // Напишите функцию printNumbers(from, to, interval), которая последовательно выводит в консоль целые числа, начиная от from и заканчивая to, с интервалом между выводом соседних чисел в interval мс.
 // Реализуйте первый или *оба варианта решения:
 
-// - Используя setInterval.
+// Используя setInterval
 
-printNumbers(1, 5, 800);
+// printNumbers1(2, 8, 500);
 
-function printNumbers(from, to, interval) {
+function printNumbers1(from, to, interval) {
   if (
     typeof from !== "number" ||
     typeof to !== "number" ||
@@ -22,11 +22,46 @@ function printNumbers(from, to, interval) {
   if (interval < 0) {
     throw new RangeError("interval не может быть < 0");
   } else {
-    let j = 1;
-    for (let i = from; i <= to; i++) {
-      setTimeout(() => {
-        console.log("Number :>> ", i);
-      }, interval * j++);
-    }
+    let argument = from;
+
+    let output = setInterval(function () {
+      console.log(argument);
+      if (argument === to) {
+        clearInterval(output);
+      }
+      argument++;
+    }, interval);
+  }
+}
+
+// *Используя рекурсивный setTimeout
+
+printNumbers2(1, 5, 800);
+
+function printNumbers2(from, to, interval) {
+  if (
+    typeof from !== "number" ||
+    typeof to !== "number" ||
+    typeof interval !== "number" ||
+    !Number.isSafeInteger(interval) ||
+    !Number.isSafeInteger(to) ||
+    !Number.isSafeInteger(from)
+  ) {
+    throw new TypeError("Все три параметра должны быть числами");
+  }
+  if (from > to) {
+    throw new RangeError("from не может быть > to");
+  }
+  if (interval < 0) {
+    throw new RangeError("interval не может быть < 0");
+  } else {
+    let current = from;
+    setTimeout(function output() {
+      console.log(current);
+      if (current < to) {
+        setTimeout(output, interval);
+      }
+      current++;
+    }, interval);
   }
 }
